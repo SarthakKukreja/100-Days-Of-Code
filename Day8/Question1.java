7ce14235960bfd3653ffa30b94a1fce08eb8f2ca
Sarthak Kukreja <Sarthak Kukreja>
2017-01-14 18:02:13 +0530

    Day8

---
+++ Day8/Question1.java
@@ -0,0 +1,19 @@
+//https://www.hackerrank.com/contests/rookierank/challenges/birthday-cake-candles
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Question1 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int height[] = new int[n];
+        for(int height_i=0; height_i < n; height_i++){
+            height[height_i] = in.nextInt();
+        }
+    }
+}
